2ef2979eb282f89e383e0281b629bdd0af071039
andersonmunyalo10350 <anderson.munyalo@strathmore.edu>
2026-06-17 10:44:40 +0300

    add letter grade assignment fee
    Updated version to 1.1 and added letter grade assignment based on average.

--- tracker.py
+++ tracker.py
@@ -1,8 +1,16 @@
-# Grade Tracker Version 1.0
+# Grade Tracker Version 1.1
 grades = [80, 90, 70, 85]
 
 def calculate_average(grade_list):
     total = sum(grade_list)
     return total / len(grade_list)
 
-print("Average Grade:", calculate_average(grades))
+# NEW FEATURE: Assign Letter Grade
+avg = calculate_average(grades)
+if avg >= 80:
+    letter = 'A'
+else:
+    letter = 'B'
+
+print("Average Grade:", avg)
+print("Final Letter Grade:", letter)
